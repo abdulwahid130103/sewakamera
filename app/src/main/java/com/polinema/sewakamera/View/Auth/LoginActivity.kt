210434b96049841a03c84b784f805b075fadaeec
abdulwahidsafiudin <yusrannganga@gmail.com>
2024-02-13 15:00:40 +0700

    update fitur login and register

--- app/src/main/java/com/polinema/sewakamera/View/Auth/LoginActivity.kt
+++ app/src/main/java/com/polinema/sewakamera/View/Auth/LoginActivity.kt
@@ -1,12 +1,157 @@
 package com.polinema.sewakamera.View.Auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.polinema.sewakamera.Helper.LoadingDialog
 import com.polinema.sewakamera.R
+import com.polinema.sewakamera.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+
+    lateinit var loadingDialog: LoadingDialog
+
+    private lateinit var b : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        b = ActivityLoginBinding.inflate(layoutInflater)
+        val view = b.root
+        setContentView(view)
+
+        textAutoCheck()
+
+        loadingDialog = LoadingDialog(this)
+
+        b.signUpTv.setOnClickListener {
+            intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        b.loginBtn.setOnClickListener {
+            checkInput()
+
+
+        }
     }
+
+    private fun textAutoCheck() {
+
+
+
+        b.emailEt.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (b.emailEt.text.isEmpty()){
+                    b.emailEt.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+
+                }
+                else if (Patterns.EMAIL_ADDRESS.matcher(b.emailEt.text).matches()) {
+                    b.emailEt.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                    b.emailError.visibility = View.GONE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+
+                b.emailEt.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if (Patterns.EMAIL_ADDRESS.matcher(b.emailEt.text).matches()) {
+                    b.emailEt.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                    b.emailError.visibility = View.GONE
+                }
+            }
+        })
+
+        b.PassEt.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (b.PassEt.text.isEmpty()){
+                    b.PassEt.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+
+                }
+                else if (b.PassEt.text.length > 4){
+                    b.PassEt.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+
+                b.PassEt.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                b.passwordError.visibility = View.GONE
+                if (count > 4){
+                    b.PassEt.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+
+                }
+            }
+        })
+
+
+
+    }
+
+    private fun checkInput() {
+
+        if (b.emailEt.text.isEmpty()){
+            b.emailError.visibility = View.VISIBLE
+            b.emailError.text = "Email tidak boleh kosong!"
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(b.emailEt.text).matches()) {
+            b.emailError.visibility = View.VISIBLE
+            b.emailError.text = "Email tidak valid!"
+            return
+        }
+        if(b.PassEt.text.isEmpty()){
+            b.passwordError.visibility = View.VISIBLE
+            b.passwordError.text = "Password tidak boleh kosong !"
+            return
+        }
+
+        if(b.PassEt.text.length < 8){
+            b.passwordError.visibility = View.VISIBLE
+            b.passwordError.text = "Minimal 8 digit"
+            return
+        }
+
+        if ( b.PassEt.text.isNotEmpty() && b.emailEt.text.isNotEmpty()){
+            b.emailError.visibility = View.GONE
+            b.passwordError.visibility = View.GONE
+//            signInUser()
+            Toast.makeText(this,"Sukses login",Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+//    private fun signInUser() {
+//
+//
+//    }
 }
--- app/src/main/java/com/polinema/sewakamera/View/Auth/RegisterActivity.kt
+++ app/src/main/java/com/polinema/sewakamera/View/Auth/RegisterActivity.kt
@@ -1,12 +1,230 @@
 package com.polinema.sewakamera.View.Auth
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.polinema.sewakamera.R
+import com.polinema.sewakamera.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var b : ActivityRegisterBinding
+    private lateinit var progressDialog: ProgressDialog
+
+
+    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        b = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = b.root
+        setContentView(view)
+
+        progressDialog = ProgressDialog(this)
+
+        textAutoCheck()
+
+        b.signInTvSignUpPage.setOnClickListener {
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        b.signUpBtnSignUpPage.setOnClickListener {
+            checkInput()
+
+        }
     }
+
+    private fun textAutoCheck() {
+
+        b.nameEtSignUpPage.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (b.nameEtSignUpPage.text.isEmpty()){
+                    b.nameEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+
+                }
+                else if (b.nameEtSignUpPage.text.length >= 1){
+                    b.nameEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                    b.fullnameError.visibility = View.GONE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+
+                b.nameEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if (count >= 4){
+
+                    b.nameEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                    b.fullnameError.visibility = View.GONE
+                }
+            }
+        })
+
+        b.emailEtSignUpPage.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (b.emailEtSignUpPage.text.isEmpty()){
+                    b.emailEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+
+                }
+                else if (b.emailEtSignUpPage.text.matches(emailPattern.toRegex())) {
+                    b.emailEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+
+                b.emailEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if (b.emailEtSignUpPage.text.matches(emailPattern.toRegex())) {
+                    b.emailError.visibility = View.GONE
+                    b.emailEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                }
+            }
+        })
+
+        b.PassEtSignUpPage.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (b.PassEtSignUpPage.text.isEmpty()){
+                    b.PassEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+
+                }
+                else if (b.PassEtSignUpPage.text.length >= 8){
+                    b.passError.visibility = View.GONE
+                    b.PassEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+
+                b.PassEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if (count > 8){
+                    b.passError.visibility = View.GONE
+                    b.PassEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                }
+            }
+        })
+
+        b.cPassEtSignUpPage.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (b.cPassEtSignUpPage.text.isEmpty()){
+                    b.cPassEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+
+                }
+                else if (b.cPassEtSignUpPage.text.length < 8){
+                    b.cPassEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+                }else if ((b.cPassEtSignUpPage.text.length >= 8) && (b.cPassEtSignUpPage.text.toString().equals(b.PassEtSignUpPage.text.toString()))){
+                    b.cpassError.visibility = View.GONE
+                    b.cPassEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+
+                b.cPassEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if ((b.cPassEtSignUpPage.text.length >= 8) && (b.cPassEtSignUpPage.text.toString().equals(b.PassEtSignUpPage.text.toString()))){
+                    b.cpassError.visibility = View.GONE
+                    b.cPassEtSignUpPage.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(applicationContext,
+                        R.drawable.ic_check
+                    ), null)
+                }
+            }
+        })
+
+    }
+
+    private fun checkInput() {
+        if (b.nameEtSignUpPage.text.isEmpty()){
+            b.fullnameError.visibility = View.VISIBLE
+            b.fullnameError.text = "Nama tidak boleh kosong!"
+            return
+        }
+        if (b.emailEtSignUpPage.text.isEmpty()){
+            b.emailError.visibility = View.VISIBLE
+            b.emailError.text = "Email tidak boleh kosong!"
+            return
+        }
+
+        if (!b.emailEtSignUpPage.text.matches(emailPattern.toRegex())) {
+            b.emailError.visibility = View.VISIBLE
+            b.emailError.text = "Email tidak valid!"
+            return
+        }
+        if(b.PassEtSignUpPage.text.isEmpty()){
+            b.passError.visibility = View.VISIBLE
+            b.passError.text = "Password tidak boleh kosong!"
+            return
+        }
+        if(b.PassEtSignUpPage.text.length < 8){
+            b.passError.visibility = View.VISIBLE
+            b.passError.text = "Minimal 8 digit"
+            return
+        }
+        if(b.cPassEtSignUpPage.text.isEmpty()){
+            b.cpassError.visibility = View.VISIBLE
+            b.cpassError.text = "Password tidak boleh kosong!"
+            return
+        }
+        if(b.cPassEtSignUpPage.text.length < 8){
+            b.cpassError.visibility = View.VISIBLE
+            b.cpassError.text = "Minimal 8 digit"
+            return
+        }
+        if (b.PassEtSignUpPage.text.toString() != b.cPassEtSignUpPage.text.toString()){
+            b.cpassError.visibility = View.VISIBLE
+            b.cpassError.text = "Password tidak cocok!"
+            return
+        }
+
+
+//        signIn()
+        Toast.makeText(this,"Sukses Register", Toast.LENGTH_LONG).show()
+
+
+    }
+
+
+
 }
