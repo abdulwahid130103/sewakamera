e27f1a40cc33459b61739da60ea33e9d1afa7dca
abdulwahidsafiudin <yusrannganga@gmail.com>
2024-07-09 22:52:26 +0700

    update connection api

--- app/src/main/java/com/polinema/sewakamera/Model/Connection.kt
+++ app/src/main/java/com/polinema/sewakamera/Model/Connection.kt
@@ -2,7 +2,7 @@ package com.polinema.sewakamera.Model
 
 class Connection {
 
-    var url_root = "http://192.168.199.161/sewakameranew/sewakameranew/public/"
+    var url_root = "http://http://Sewakamerakediri.my.id/"
     var url_login = "${url_root}api/login"
     var url_cover_produk = "${url_root}api/get_produk"
     var url_cover_produk_terbaru = "${url_root}api/get_produk_terbaru"
